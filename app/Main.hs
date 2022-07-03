230be86080bc43150064336e3582157e01eaa374
jim <jim@pop-os.localdomain>
2022-07-03 00:28:43 -0500

    end of day update, can now read in and encode data with string values

--- app/Main.hs
+++ app/Main.hs
@@ -12,66 +12,59 @@ import Data.Scientific
 import Control.Applicative (empty)
 import qualified Data.ByteString.Lazy.Char8 as BL
 import Data.Aeson.Encode.Pretty (encodePretty)
+import System.Environment (getArgs)
+import Data.List.Split ( split, splitOn )
+import Data.Aeson.Encoding (pair)
+import Data.Text (pack)
+import Data.Aeson.Key
+-- import System.Posix.Env.ByteString (getArgs)
+-- import Data.ByteString (split, ByteString)
+-- import Data.ByteString.Lazy (toChunks)
 
--- data Coord = Coord { x :: Double, y :: Double } deriving (Show)
+splitArgs :: Eq a => [a] -> [[a]] -> [[[a]]]
+splitArgs delim [] = []
+splitArgs delim [x] = splitOn delim x : splitArgs delim []
+splitArgs delim (x:xs) = splitOn delim x : splitArgs delim xs
 
--- data KVPair = KVPair { k :: String, v :: Int} deriving (Show)
-
--- data KVPair_ = KVPair_ { k_ :: String, v_ :: Int} deriving (Show)
-
--- -- core data type
--- -- data Value 
--- --   = Object Object
--- --   | Array Array
--- --   | String Text
--- --   | Number Scientific
--- --   | Bool Bool
--- --   | Null
-
--- b = "test"
--- c = 12345
--- customValue :: Value
--- customValue = object 
---   [
---     "list_price" .= (15000 :: Int)
---     , b .= (c :: Int)
---   ]
-
--- -- A ToJSON instance allows us to encode a value as JSON.
-
--- instance ToJSON Coord where
---   toJSON (Coord xV yV) = object [ "x" .= xV,
---                                   "y" .= yV ]
-
---   toEncoding Coord{..} = pairs $
---     "x" .= x <>
---     "y" .= y
-
--- instance ToJSON KVPair where 
---   toJSON (KVPair kV vV) = object [ "key" .= kV,
---                                    "val" .= vV ]
-
--- A FromJSON instance allows us to decode a value from JSON.  This
--- should match the format used by the ToJSON instance.
-
--- instance FromJSON Coord where
---   parseJSON (Object v) = Coord <$>
---                          v .: "x" <*>
---                          v .: "y"
---   parseJSON _          = empty
-
--- data Value 
---   = Object Object
---   | Array Array
---   -- | String Text
---   | Number Scientific
---   | Bool Bool
---   | Null
-
-data Num = Num Float | Catdog Int deriving Show
+buildPairList :: [[String]] -> [(Key, Value)]
+buildPairList [] = []
+buildPairList [x] = [(k, v)]
+  where 
+    k = fromString $ head x
+    v = String $ pack $ last x
+buildPairList (x:xs) = (k, v) : buildPairList xs
+  where 
+    k = fromString $ head x
+    v = String $ pack $ last x
 
 main :: IO ()
 main = do
+  -- get the args from the command line
+  args <- getArgs
+
+  -- define a delim
+  let delim = "="
+
+  -- split this into a list of pairs
+  let listOfPairs = buildPairList $ splitArgs delim args
+
+  let o = object listOfPairs
+
+  BL.putStrLn $ encodePretty o
+
+  -- old test driver code below
+
+  -- lets make a pair.
+  -- a pair is a key/value pair for an Object. 
+  -- type Pair = (Text, Value).  We will need to do some conversion for this to work!
+
+  -- let k = fromString $ head aPair
+  -- let v = String $ pack $ last aPair
+  -- let myPair = (k, v)
+  -- let pleaseWork = [myPair]
+
+
+
   -- let req = decode "{\"x\":3.0,\"y\":-1.0}" :: Maybe Coord
   -- let t = KVPair "bruh" 2
   -- let k_ = KVPair "test" 1
@@ -82,18 +75,26 @@ main = do
 
   -- BL.putStrLn (encode reply)
   -- BL.putStrLn (encode t)
-  
-  let v0 = String "cody"
-  let v1 = Bool True
-  let v2 = Number 1
-  let v3 = Number 1.234
-  let v5 = Null
 
-  let o = object [
-                    "name" .= v0
-                  , "isDoggy"  .= v1
-                  , "flips" .= v2
-                  , "flips_precise" .= v3
-                  , ("nuggets" .= v5)
-                ]
-  BL.putStrLn (encodePretty o)
+  -- let v0 = String "cody"
+  -- let v1 = Bool True
+  -- let v2 = Number 1
+  -- let v3 = Number 1.234
+  -- let v5 = Null
+
+  -- let o = object [
+  --                   "name" .= v0
+  --                 , "isDoggy"  .= v1
+  --                 , "flips" .= v2
+  --                 , "flips_precise" .= v3
+  --                 -- , ("nuggets" .= v5)
+  --               ]
+
+  -- let o2 = object [
+  --                   "nestedDataToo!" .= v1
+  --                   , "nestedObject" .= o
+  --                 ]
+
+  -- BL.putStrLn (encodePretty o2)
+
+
